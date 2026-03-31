cc2e22df5825d86bdcb934b990c6c891a9c4c1a2
Namrata Banerji <54425214+namratabanerji@users.noreply.github.com>
2026-03-31 17:11:38 -0500

    Refactor home function to improve readability

--- app/server.py
+++ app/server.py
@@ -19,4 +19,8 @@ templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"request": request},
+    )
